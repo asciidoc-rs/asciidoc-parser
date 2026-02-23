6dfd62278483cad0ad7c9bf34f1e69d82e74e3c1
Eric Scouten <git@scouten.me>
2026-02-23 18:05:56 +0200

    Skip formatter for deeply-nested data structures

--- parser/src/tests/asciidoc_lang/lists/unordered.rs
+++ parser/src/tests/asciidoc_lang/lists/unordered.rs
@@ -895,6 +895,7 @@ See xref:separating.adoc[] for more details.
     }
 }
 
+#[rustfmt::skip] // Used because this mod has deeply-nested data structures that cause the formatter to slow down significantly.
 mod nested {
     use std::collections::HashMap;
 
